161f7be69cd4526833809cd9bef9ddfc9254a26a
Gabriel Favero Hoffmann <gabriel290974@gmail.com>
2025-10-03 15:20:44 -0300

    Remoção

--- app/Controllers/IndexController.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once '../core/Controller.php';
-
-    class IndexController extends Controller {
-        public function index() {
-            $this->view('home/index');
-        }
-    }
