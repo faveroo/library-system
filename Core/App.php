fa20c464b83e7b9fc92f473950a800232ef333ba
Gabriel Favero Hoffmann <gabriel290974@gmail.com>
2025-10-08 11:26:30 -0300

    Retirando comentário do try-catch da chamada de método dinâmico

--- Core/App.php
+++ Core/App.php
@@ -30,14 +30,14 @@ class App {
         $this->params = $url ? array_values($url) : [];
         
         // Chama o método com os parâmetros
-        // try {
+        try {
             call_user_func_array([$this->controller, $this->method], $this->params);
-        // } catch (Throwable $e) {
-        //     error_log($e);
-        //     $_SESSION['ultima_rota'] = $_SERVER['HTTP_REFERER'] ?? '/';
-        //     header("Location: " . BASE_URL . "auth/error500");
-        //     exit;
-        // }
+        } catch (Throwable $e) {
+            error_log($e);
+            $_SESSION['ultima_rota'] = $_SERVER['HTTP_REFERER'] ?? '/';
+            header("Location: " . BASE_URL . "auth/error500");
+            exit;
+        }
 
     }
     
